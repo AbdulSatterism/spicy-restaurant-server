3df99a06af563c9a5b4e855e06965432fa06bbfc
Md. Abdul Satter <116937243+AbdulSatterism@users.noreply.github.com>
2023-10-25 21:24:52 +0600

    some dashbord emplement

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 const express = require('express')
 const app = express();
 const cors = require('cors');
+const jwt = require('jsonwebtoken');
 require('dotenv').config();
 const port = process.env.PORT || 5000
 
@@ -25,11 +26,51 @@ async function run() {
         const menuCollection = client.db('spicyRestaurant').collection('menu');
         const reviewCollection = client.db('spicyRestaurant').collection('reviews');
         const cartCollection = client.db('spicyRestaurant').collection('carts');
+        const usersCollection = client.db('spicyRestaurant').collection('users');
 
+        //users related apis
+
+        app.get('/users', async (req, res) => {
+            const result = await usersCollection.find().toArray();
+            res.send(result)
+        })
+
+        app.post('/users', async (req, res) => {
+            const user = req.body;
+            const query = { email: user?.email };
+            const existingUser = await usersCollection.findOne(query);
+            if (existingUser) {
+                return res.send({ message: 'User already exists' })
+            }
+            const result = await usersCollection.insertOne(user)
+            res.send(result)
+        });
+
+        app.patch('/users/admin/:id', async (req, res) => {
+            const id = req.params.id;
+            const filter = { _id: new ObjectId(id) };
+            const updateDoc = {
+                $set: {
+                    role: 'admin'
+                },
+            };
+            const result = await usersCollection.updateOne(filter, updateDoc);
+            res.send(result)
+        });
+
+        app.delete('/users/:id', async (req, res) => {
+            const id = req.params.id;
+            const query = { _id: new ObjectId(id) };
+            const result = await usersCollection.deleteOne(query);
+            res.send(result)
+        })
+
+        //menu related
         app.get('/menu', async (req, res) => {
             const result = await menuCollection.find().toArray();
             res.send(result)
         })
+        //review related
         app.get('/reviews', async (req, res) => {
             const result = await reviewCollection.find().toArray();
             res.send(result)
